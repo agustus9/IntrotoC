d5f7aea68b150aa8fb2c3ba402330d193cbd97d0
Mack <amack@proforma.com>
2018-04-03 18:35:20 -0400

    My first C# program

--- IntrotoC/Program.cs
+++ IntrotoC/Program.cs
@@ -11,13 +11,29 @@ namespace IntrotoC
         static void Main(string[] args)
         {
             int dogs = 10;
+            Console.WriteLine("There are " + dogs + " dogs in the park");
+            Console.Read();
             int sandwiches = 3;
+            Console.WriteLine("I can eat " + sandwiches + " sandwiches");
+            Console.Read();
             double dinner = 5.75;
+            Console.WriteLine("Our dinner costs " + dinner);
+            Console.Read();
             string carColor = "green";
+            Console.WriteLine("The color of my car is " + carColor);
+            Console.Read();
             string carMake = "Honda";
+            Console.WriteLine("I have a " + carMake);
+            Console.Read();
             string model = "Jennifer";
+            Console.WriteLine("My favorite model name is " + model + " Accord");
+            Console.Read();
             string firstLetter = "A";
-            int stars = 
+            Console.WriteLine("The firstLetter in my name is " + firstLetter);
+            Console.Read();
+            int stars = 3000;
+            Console.WriteLine("There are " + stars + " in the sky");
+            Console.Read();
         }
     }
 }
